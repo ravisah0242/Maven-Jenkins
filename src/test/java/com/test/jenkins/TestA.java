4145e6d67f1410f9dd0dbd262d0ccf42922cb77b
Ravi <Ravi@DESKTOP-9P1MDVM>
2021-02-12 12:57:22 +0530

    jenkins

---
+++ src/test/java/com/test/jenkins/TestA.java
@@ -0,0 +1,23 @@
+package com.test.jenkins;
+
+import org.testng.annotations.Test;
+
+public class TestA 
+{
+	@Test
+	public void sania()
+	{
+		System.out.println("Sanaya");
+	}
+	
+	@Test
+	public void richa()
+	{
+		System.out.println("Richa");
+	}
+	@Test
+	public void reena()
+	{
+		System.out.println("Reena");
+	}
+}
---
+++ src/test/java/com/test/jenkins/TestB.java
@@ -0,0 +1,31 @@
+package com.test.jenkins;
+
+import java.sql.Driver;
+
+import org.testng.annotations.Test;
+
+public class TestB
+{
+	@Test
+	public void ravi()
+	{
+		System.out.println("Ravi");
+	}
+	@Test
+	public void utkarsh()
+	{
+		System.out.println("Utkarsh");
+	}
+	@Test
+	public void pawan()
+	{
+		System.out.println("Pawan");
+	}
+	
+	@Test
+	public void Amit()
+	{
+		System.out.println(Driver.class);
+	}
+
+}
